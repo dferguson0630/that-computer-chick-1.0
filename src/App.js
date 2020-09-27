039cf427840505426f7570fe99ac2e7c74f98782
dferguson0630 <47204218+kanyetehgiant@users.noreply.github.com>
2020-09-27 10:07:19 -0400

    browser router property changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div>
           <Route path="/" exact component={Home}/>
           <Route path="/services" component={Services}/>
