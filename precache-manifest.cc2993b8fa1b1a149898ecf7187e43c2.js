f40591dc8f7c9c7b061dc5dec7e1118871ca3d39
dferguson0630 <47204218+kanyetehgiant@users.noreply.github.com>
2020-09-27 08:47:38 -0400

    Updates

---
+++ precache-manifest.cc2993b8fa1b1a149898ecf7187e43c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eac4b95c587ef481fb2064d6fd427ef",
+    "url": "/that-computer-chick-1.0/index.html"
+  },
+  {
+    "revision": "7bebee98eac2723024ae",
+    "url": "/that-computer-chick-1.0/static/css/main.b8090f5f.chunk.css"
+  },
+  {
+    "revision": "886793c31362099ed79f",
+    "url": "/that-computer-chick-1.0/static/js/2.629bf476.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/that-computer-chick-1.0/static/js/2.629bf476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bebee98eac2723024ae",
+    "url": "/that-computer-chick-1.0/static/js/main.c13c588e.chunk.js"
+  },
+  {
+    "revision": "0fc6cb066a911f6614f4",
+    "url": "/that-computer-chick-1.0/static/js/runtime-main.d91b57df.js"
+  },
+  {
+    "revision": "ac119413358de24c848cf1fcb5bd0f0f",
+    "url": "/that-computer-chick-1.0/static/media/Storefront.ac119413.jpg"
+  },
+  {
+    "revision": "dd2bcfeaf950cfb327080f3f0e36bdae",
+    "url": "/that-computer-chick-1.0/static/media/about-us.dd2bcfea.jpg"
+  },
+  {
+    "revision": "d0c1ce312a3e89ef1442943ac520fb92",
+    "url": "/that-computer-chick-1.0/static/media/about.d0c1ce31.jpg"
+  },
+  {
+    "revision": "555d9e76e1e81b7c89183b73ec072ec5",
+    "url": "/that-computer-chick-1.0/static/media/atlanta.555d9e76.jpg"
+  },
+  {
+    "revision": "259363bfc2e84887a3ae0faf230f8361",
+    "url": "/that-computer-chick-1.0/static/media/store1.259363bf.jpg"
+  },
+  {
+    "revision": "afc1a088441e0ede83b5e3b93debb9d8",
+    "url": "/that-computer-chick-1.0/static/media/watch.afc1a088.jpg"
+  }
+]);
